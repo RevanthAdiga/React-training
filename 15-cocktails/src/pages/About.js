c9024de5ab51df7320a83ece438ce7a85023e734
revanth.adiga@tigeranalytics.com <revanth.adiga@tigeranalytics.com>
2022-10-17 15:11:51 +0530

    additional projects

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi facilis, voluptatem necessitatibus eligendi ea, quia placeat quo ad corporis quisquam dicta aspernatur architecto doloremque libero. Dolorum cumque eos quibusdam architecto animi debitis. Repudiandae, non ducimus? Natus, perspiciatis repellendus. Eveniet, ducimus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
